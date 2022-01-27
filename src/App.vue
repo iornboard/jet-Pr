b21d9841a91261426b6df8334604291ca382074c
DESKTOP-U2P2DON\KUCK_SU <kuck1106@naver.com>
2022-01-27 20:23:59 +0900

    devlog set module & dir by nam

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
-    <b-button>Button</b-button>
   </div>
 </template>
 
--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -14,9 +14,15 @@ module.exports = {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
   },
+  // 테스트 코드를 찿지 않을 경로 설정
   testPathIgnorePatterns: [
-    '<rootDir>/test/e2e'
+    '<rootDir>/test/e2e',
+    '/node_modules/'
   ],
+  // 테스트 코드의 특정 설정
+  testMatch: ['**/?(*.)+(spec|test).ts'],
+  // 테스트 코드가 동작되는 최대 시간
+  testTimeout: 100000,
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
   mapCoverage: true,
