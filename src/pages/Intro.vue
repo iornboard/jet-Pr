06c1b0c9a4594711c8e89794203d026c6a4fd850
DESKTOP-U2P2DON\KUCK_SU <kuck1106@naver.com>
2022-02-02 20:57:50 +0900

    devlog set vue-router

---
+++ src/pages/Intro.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="root">
+    <h1>{{ msg }}</h1>
+    <br/><br/><br/>
+    <b-container class="bv-example-row">
+    <b-row class="text-center">
+        <b-col>
+        </b-col>
+        <b-col cols="8">
+            <div id="dropBar">
+                <b-form-file id="file-default"></b-form-file>
+            </div>
+        </b-col>
+        <b-col>
+        </b-col>
+    </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'intro',
+  data () {
+    return {
+      msg: 'Input your xml file'
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+#dropBar {
+  width: 100%;
+  height: 50vh;
+  border-style: solid;
+  padding: 10PX;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import Intro from '@/pages/Intro'
+import Output from '@/pages/TestOutput'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history', // 히스토리 모드로 설정 <-> hash
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Intro',
+      component: Intro
+    },
+    {
+      path: '/output',
+      name: 'output',
+      component: Output
     }
   ]
 })
