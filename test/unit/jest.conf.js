e5fd8604d4d945fc25ba5052369e9563ef06de53
DESKTOP-U2P2DON\KUCK_SU <kuck1106@naver.com>
2022-02-11 21:04:32 +0900

    devlog set jest test

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -20,9 +20,9 @@ module.exports = {
     '/node_modules/'
   ],
   // 테스트 코드의 특정 설정
-  testMatch: ['**/?(*.)+(spec|test).ts'],
+  // testMatch: ['**/?(*.)+(spec|test).ts'],
   // 테스트 코드가 동작되는 최대 시간
-  testTimeout: 100000,
+  // testTimeout: 100000,
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
   mapCoverage: true,
@@ -32,5 +32,7 @@ module.exports = {
     '!src/main.js',
     '!src/router/index.js',
     '!**/node_modules/**'
-  ]
+  ],
+  verbose: true,
+  testURL: 'http://localhost/'
 }
